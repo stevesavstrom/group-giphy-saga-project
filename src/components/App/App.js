37fe52312486f7c5ead9c78132018aeb7907f667
Kashia Moua <kserenitym@gmail.com>
2021-07-22 16:40:49 -0500

    created table, set up handler, imported search into app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { HashRouter as Router, Route, Link } from 'react-router-dom'
-
+import Search from '../Search/Search';
 
 function App(props) {
   return (
     <Router>
       <div>
         <h1>Giphy Search!</h1>
-        {/* <Route path="/" exact>
+          <Route path="/" exact>
             <Search />
           </Route>
-          <Route path="/Favorite">
+          {/* <Route path="/Favorite">
             <p>Favorite:</p>
-          <Favorite />
-        </Route> */}
+          <Favorite /> */}
+        {/* </Route> */}
       </div>
     </Router>
   );
